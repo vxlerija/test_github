f4c60675c79d3338b98d868189c2974358211c88
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-01-16 09:39:51 +0100

    attributes with getter and setter, toString and methods added

--- src/geoMetry/Drawable.java
+++ src/geoMetry/Drawable.java
@@ -2,4 +2,31 @@ package geoMetry;
 
 public abstract class Drawable {
 
+	private double x;
+	private double y;
+	
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Drawable [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+	public abstract double circumference();
+	public abstract double area();
 }
