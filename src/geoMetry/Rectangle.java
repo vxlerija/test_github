783419a0e72a3cc3cca5018fce7a8a44f41d4638
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-01-16 09:45:10 +0100

    attributes, getter and setter aswell as method for circumference and area added

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -1,5 +1,33 @@
 package geoMetry;
 
-public class Rectangle {
+public class Rectangle extends Drawable{
 
+	private double length;
+	private double width;
+	
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	
+	@Override
+	public double circumference() {
+		return this.length*2 + this.width*2;
+	}
+	@Override
+	public double area() {
+		return this.length*this.width;
+	}
+	
+	
 }
