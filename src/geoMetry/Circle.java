5d63182281bc65cd4472ab45f62eac5c1af70053
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-01-22 11:05:07 +0100

    attribute with getter and setter added as well as Methods from Drawable

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -1,5 +1,28 @@
 package geoMetry;
 
-public class Circle {
+public class Circle extends Drawable {
+	
+	private double radius;
 
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double circumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 }
