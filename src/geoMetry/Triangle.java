27757d062117f05ba2c056afee5a7c095ed0853d
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-01-22 11:07:08 +0100

    attributes added to Triangle as well as getter, setter and methods from Drawable

--- src/geoMetry/Triangle.java
+++ src/geoMetry/Triangle.java
@@ -1,5 +1,34 @@
 package geoMetry;
 
-public class Triangle {
+public class Triangle extends Drawable {
 
+	private double length;
+	private double hight;
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getHight() {
+		return hight;
+	}
+	public void setHight(double hight) {
+		this.hight = hight;
+	}
+	
+	
+	@Override
+	public double circumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 }
