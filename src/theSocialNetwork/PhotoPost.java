400f24539f4ccf22604390bc82b3c650ab61fea6
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-02-06 09:05:03 +0100

    attribute "headline" added and code corrected

--- src/theSocialNetwork/PhotoPost.java
+++ src/theSocialNetwork/PhotoPost.java
@@ -3,6 +3,7 @@ package theSocialNetwork;
 public class PhotoPost extends Messages {
 
 	private String dataname;
+	private String headline;
 
 	public String getDataname() {
 		return dataname;
@@ -12,8 +13,22 @@ public class PhotoPost extends Messages {
 		this.dataname = dataname;
 	}
 	
+	public String getHeadline() {
+		return headline;
+	}
+	
+	public void setHeadline(String headline) {
+		this.headline = headline;
+	}
+	
 	public String toString() {
-		return super.toString() + "\n\t" + "text: " + this.dataname;
+		return super.toString() + "\n\t" + "text: " + this.dataname + "\n\t" + "headline: " + this.headline;
+	}
+
+	@Override
+	public void addLike() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
