372c3838e9f1d0b620fdb1f98be725afe0ea98f2
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-02-20 09:35:45 +0100

    Task2 completed

--- src/array/Task2.java
+++ src/array/Task2.java
@@ -1,6 +1,18 @@
 package array;
 
 public class Task2 {
+	
 
 	
+	public boolean isProbablyApproaching(String[] signs) {
+		if(signs.length == 0) {
+			System.out.println("Array is null");
+			return false;
+		}
+		
+		for (int i = 0; i < signs.length-3; i++) {
+			if (signs[i].equals(signs[i+1])&&equals(signs[i+2])&&equals(signs[i+3])) {
+			}		}
+		return true;
+	}
 }
