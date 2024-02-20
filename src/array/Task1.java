4de984e358f48ca140fab0d719a2605ece26baeb
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-02-20 09:35:32 +0100

    if statement added

--- src/array/Task1.java
+++ src/array/Task1.java
@@ -5,6 +5,11 @@ public class Task1 {
 	
 		public static int count5PercentJumps(int[] gains) {
 			int numberOfJumps = 0;
+			if(gains.length == 0) {
+				System.out.println("Array is null");
+				return 0;
+			}
+			
 			for (int i = 0; i < gains.length-1; i++) {
 				if (gains[i] == gains[i+1] * 1.05) {
 				numberOfJumps ++;
