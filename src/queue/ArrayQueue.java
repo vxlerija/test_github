01c4753c982bd0e3b223eb2b9b15a46e809f44f8
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-03-12 09:59:44 +0100

    Queue

---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,34 @@
+package queue;
+
+public class ArrayQueue {
+	
+	private static int front;
+	private static int rear;
+	private static int capacity;
+	private static int[] queue;
+	
+	public ArrayQueue(int size) {
+		this.front = 0;
+		this.rear = 0;
+		this.capacity = size;
+		queue = new int[size];
+	}
+	
+	public static void queueEnqueue(int item) {
+		if (rear == capacity) {
+			System.out.println("Queue ist voll");
+		} else {
+			queue[rear] = item;
+			rear++;
+		}
+	}
+	
+	public static int queueDequeue() {
+		if(rear == front) {
+			System.out.println("Queue ist leer");
+		} else {
+			int firstElement = queue[front];
+			
+		}
+	}
+}
